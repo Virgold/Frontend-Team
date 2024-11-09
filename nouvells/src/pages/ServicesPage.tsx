0c0444d2c2c54a37dce236e6032e08aef00d5a4b
TheSaviourEking <saviour.eking@outlook.com>
2024-11-09 07:42:44 +0100

    chore: add service page

--- nouvells/src/pages/ServicesPage.tsx
+++ nouvells/src/pages/ServicesPage.tsx
@@ -1,11 +1,36 @@
+import CategoriesSwiper from "@/components/CategoriesSwiper"
 import Hero from "@/components/Hero"
+import JobsLists from "@/components/JobsLists";
+
+import { Search } from 'lucide-react';
+import { useState } from "react";
 
 const ServicesPage = () => {
+    const [activeCategory, setActiveCategory] = useState('Graphics & Design');
+
     return (
         <>
-            <section className="sectio">
-                <Hero page='services' />
-            </section>
+            <Hero page='services' />
+            <CategoriesSwiper activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+
+            {/* Second Search */}
+            <div className="container">
+                <div className="sub-container">
+                    <div className="flex items-center gap-4 group border-2 focus-within:border-blue-500 focus-within:shadow-lg focus-within:bg-white rounded-l">
+                        <Search />
+                        <input
+                            type="text"
+                            name="search"
+                            id="longerSearch"
+                            placeholder="Job, Category, Keyword, Company..."
+                            className="w-full py-2 focus:outline-none focus:border-primary"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {/*  */}
+            <JobsLists category={activeCategory} />
         </>
     )
 }
