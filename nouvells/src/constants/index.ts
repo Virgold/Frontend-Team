20349ab6bfd5a0619873e1c6be9a50dc46059d40
TheSaviourEking <saviour.eking@outlook.com>
2024-11-09 07:41:53 +0100

    chore: update the app CONSTANTS

--- nouvells/src/constants/index.ts
+++ nouvells/src/constants/index.ts
@@ -5,9 +5,29 @@ export const initialState: ThemeProviderState = {
     setTheme: () => null,
 }
 
-export const navLinks = [
+export const NAVLINKS = [
     { href: '/jobs', label: 'Jobs' },
     { href: '/company', label: 'Company' },
     { href: '/services', label: 'Services' },
     { href: '/pricing', label: 'Pricing' }
 ];
+
+export const JOB_CATEGORIES = [
+    'Graphics & Design',
+    'Programming & Tech',
+    'Development',
+    'Digital Marketing',
+    'IT & Consultancy',
+    'Business',
+    'Lifestyle',
+    'Writing',
+    'Music & Audio',
+    'Video & Animation',
+    'More Services'
+];
+
+export enum JOB_DISPLAY_COUNT {
+    large = 5,
+    tablet = 6,
+    small = 3
+}
