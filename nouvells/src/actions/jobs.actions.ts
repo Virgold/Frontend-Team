c9d8c31c1285854e8c6489c848ae28a79bd675b7
TheSaviourEking <saviour.eking@outlook.com>
2024-11-10 18:58:27 +0100

    chore: add global search

--- nouvells/src/actions/jobs.actions.ts
+++ nouvells/src/actions/jobs.actions.ts
@@ -1,14 +1,25 @@
 import jobs from '@/../data/jobs.json';
 
-export const getJobs = async (categoryIndex: number) => {
-    if (categoryIndex) {
-        const filteredJobs = jobs.filter((job, index) => index === categoryIndex);
+export const getJobs = async (searchQuery?: string) => {
+    if (searchQuery && searchQuery !== 'all jobs') {
+        const filteredJobs = jobs.filter(job =>
+            job.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.company.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.category.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.sub_category.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.skills.some(skill =>
+                skill.toLowerCase().includes(searchQuery.toLowerCase())
+            ) ||
+            job.requirements.some(requirement =>
+                requirement.toLowerCase().includes(searchQuery.toLowerCase())
+            ) ||
+            job.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.employment_type.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.location.type.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            job.status.toLowerCase().includes(searchQuery.toLowerCase())
+        )
         return filteredJobs;
     }
 
     return jobs;
 }
-
-// export const getJobs = async () => {
-//     return jobs;
-// }
