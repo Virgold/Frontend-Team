3a4fd6389cab47a55d4910ad5165672dec9daa89
TheSaviourEking <saviour.eking@outlook.com>
2024-11-10 19:00:39 +0100

    chore: add ComingSoon, UnderConstructionPage and update HomePage and JobsPage

--- nouvells/src/pages/JobsPage.tsx
+++ nouvells/src/pages/JobsPage.tsx
@@ -2,38 +2,30 @@ import CategoriesSwiper from "@/components/CategoriesSwiper"
 import Hero from "@/components/Hero"
 import JobsLists from "@/components/JobsLists";
 
-import { Search } from 'lucide-react';
-import { useState } from "react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
-const ServicesPage = () => {
-    const [activeCategory, setActiveCategory] = useState(0);
-    // const [searchValue] = 
+const JobsPage = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const searchQuery = searchParams.get('query')?.toLowerCase();
+
+    useEffect(() => {
+        if (searchQuery) {
+            searchParams.set('query', searchQuery);
+            setSearchParams(searchParams);
+        } else {
+            console.log('no query');
+        }
+    }, [searchQuery, setSearchParams, searchParams]);
 
     return (
         <>
             <Hero page='jobs' />
-            <CategoriesSwiper activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+            <CategoriesSwiper searchQuery={searchQuery} />
 
-            {/* Second Search */}
-            <div className="container">
-                <div className="sub-container">
-                    <div className="flex items-center gap-4 group border-2 focus-within:border-blue-500 focus-within:shadow-lg focus-within:bg-white rounded-l">
-                        <Search />
-                        <input
-                            type="text"
-                            name="search"
-                            id="longerSearch"
-                            placeholder="Job, Category, Keyword, Company..."
-                            className="w-full py-2 focus:outline-none focus:border-primary"
-                        />
-                    </div>
-                </div>
-            </div>
-
-            {/*  */}
-            <JobsLists categoryIndex={activeCategory} />
+            <JobsLists searchQuery={searchQuery} />
         </>
     )
 }
 
-export default ServicesPage
+export default JobsPage
