9a17e1c88bd6a0318483430190eb8302b08a56fd
TheSaviourEking <saviour.eking@outlook.com>
2025-01-30 17:50:25 +0100

    refactor: rename firebase.js to firebase.ts and update configuration to use environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoQYj6f7cupH_nX53HB8mw1PUXYwHSUmA",
-  authDomain: "nouvells-waitlist.firebaseapp.com",
-  databaseURL: "https://nouvells-waitlist-default-rtdb.firebaseio.com",
-  projectId: "nouvells-waitlist",
-  storageBucket: "nouvells-waitlist.firebasestorage.app",
-  messagingSenderId: "79711553322",
-  appId: "1:79711553322:web:66565e2caa2f4ef37bc053",
-  measurementId: "G-ZYKBPD9W8C",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
